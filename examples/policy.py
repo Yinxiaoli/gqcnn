9d169a0086e7d5f849271ad95e6c247ba8a3e722
Yinxiao Li <yinxiaoli713@gmail.com>
2017-10-14 21:18:13 -0700

    Add command line output info.

--- examples/policy.py
+++ examples/policy.py
@@ -178,8 +178,12 @@ if __name__ == '__main__':
         vis.grasp(action.grasp, scale=1.5, show_center=False, show_axis=True)
         vis.title('Planned grasp on depth (Q=%.3f)' %(action.q_value))
         vis.show()
+        
+        print '*********************************************************'
         print 'grasp center=', action.grasp.center
         print 'grasp depth=', action.grasp.depth
+        print 'grasp angle=', action.grasp.angle
+        print 'grasp width=', action.grasp.width
 
         file = open('grasp.txt', 'w')
         file.write('%.2f %.2f\n' % (action.grasp.center.x, action.grasp.center.y))
